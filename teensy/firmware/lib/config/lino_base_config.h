ec610ecdc457efb3bc2a64b94271cf9b27c5870c
gssabu <87628438+gssabu@users.noreply.github.com>
2022-01-28 04:27:04 -0600

    Update lino_base_config.h

--- teensy/firmware/lib/config/lino_base_config.h
+++ teensy/firmware/lib/config/lino_base_config.h
@@ -14,23 +14,23 @@
 // #define USE_ESC
 
 //uncomment the IMU you're using
-#define USE_GY85_IMU
+//#define USE_GY85_IMU
 // #define USE_MPU6050_IMU
 // #define USE_MPU9150_IMU
-// #define USE_MPU9250_IMU
+#define USE_MPU9250_IMU
 
 #define DEBUG 1
 
-#define K_P 0.6 // P constant
+#define K_P 0.7 // P constant
 #define K_I 0.3 // I constant
-#define K_D 0.5 // D constant
+#define K_D 0.0015 // D constant
 
 //define your robot' specs here
-#define MAX_RPM 330               // motor's maximum RPM
-#define COUNTS_PER_REV 1550       // wheel encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.10       // wheel's diameter in meters
+#define MAX_RPM 80               // motor's maximum RPM
+#define COUNTS_PER_REV 3576       // wheel encoder's no of ticks per rev
+#define WHEEL_DIAMETER 0.70       // wheel's diameter in meters
 #define PWM_BITS 8                // PWM Resolution of the microcontroller
-#define LR_WHEELS_DISTANCE 0.235  // distance between left and right wheels
+#define LR_WHEELS_DISTANCE 0.185  // distance between left and right wheels
 #define FR_WHEELS_DISTANCE 0.30   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
 #define MAX_STEERING_ANGLE 0.415  // max steering angle. This only applies to Ackermann steering
 
